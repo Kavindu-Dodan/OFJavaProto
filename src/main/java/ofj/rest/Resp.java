38ab6dca6b50c11279ed4768cb23209c85c20721
Kavindu Dodanduwa <kavindudodanduwa@gmail.com>
2022-11-15 16:10:27 -0800

    working stage

--- src/main/java/ofj/rest/Resp.java
+++ src/main/java/ofj/rest/Resp.java
@@ -1,8 +1,7 @@
 package ofj.rest;
 
-
+import dev.openfeature.contrib.providers.flagd.FlagdProvider;
 import dev.openfeature.javasdk.Client;
-import dev.openfeature.javasdk.NoOpProvider;
 import dev.openfeature.javasdk.OpenFeatureAPI;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +16,21 @@ public class Resp {
 
     private static String makeResponse() {
         OpenFeatureAPI ofInstance = OpenFeatureAPI.getInstance();
-        ofInstance.setProvider(new NoOpProvider());
+
+        // NO OP
+        //ofInstance.setProvider(new NoOpProvider());
+
+        // Flag D
+        FlagdProvider provider = new FlagdProvider();
+        OpenFeatureAPI.getInstance().setProvider(provider);
 
         Client client = ofInstance.getClient();
 
-        client.getStringDetails()
+        if (client.getBooleanValue("RSP", false)) {
+            return "Welcome Home .!!";
+        }else{
+            return "Welcome home";
+        }
 
-        return client.getStringValue("RSP", "Hello <>");
     }
 }
